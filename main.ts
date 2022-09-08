d5d8432870dc378ac0b10474ab1aba09609fcdc4
EstebanGandara05 <112582675+EstebanGandara05@users.noreply.github.com>
2022-09-07 18:20:27 -0700

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-let suma = 0
-input.onButtonPressed(Button.A, function () {
-    suma = 0 + 1
-    basic.showNumber(suma)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(texto)
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(numero)
+    numero += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    texto = "oko"
+})
+input.onButtonPressed(Button.B, function () {
+    numero += -1
+    basic.showNumber(numero)
+})
+let numero = 0
+let texto = ""
+texto = "Jamón"
+numero = 34
